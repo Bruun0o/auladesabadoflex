b5420d692a905cef6141420ecfe3bda324471fd3
bruun0o <bruno.costr@gmail.com>
2024-05-18 11:58:22 -0400

    aula18

---
+++ vendo/js/faleConosco.js
@@ -0,0 +1,5 @@
+let Ouutros = document.getElementById ('outros')
+
+let Gênero = document.getElementsByName ('genero')
+let estados = document.querySelector('#estados')
+let pais = document.getElementById('pais')
